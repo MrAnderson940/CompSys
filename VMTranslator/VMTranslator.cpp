43c21efaaf2267043ae37960d8adb5b722664f69
MrAnderson940 <mranderson@MSI.localdomain>
2025-10-19 13:40:45 +1030

    where shit is going

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -160,7 +160,7 @@ string VMTranslator::vm_eq(){
     output.append("A=A-1\n");
     output.append("D=D-M\n");
     output.append("@true\n");
-    output.append("D;JEQ\n");
+    output.append("D;JNE\n");
     output.append("@0\n");
     output.append("D=A\n");
     output.append("@SP\n");
