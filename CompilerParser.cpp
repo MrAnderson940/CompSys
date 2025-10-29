2b59eff5cc00c29e3d0545bd96f7551d912e97d0
MrAnderson940 <162088152+MrAnderson940@users.noreply.github.com>
2025-10-29 17:27:25 +1030

    Add files via upload
    odor change

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -30,7 +30,7 @@ ParseTree* CompilerParser::compileProgram() {
         //std::cout << "1a" << std::endl << current()->getType() << std::endl;
         if(current()->getType() == "identifier" || current()->getValue() == "Main" || current()->getValue() == "main"){
             //std::cout << "1b" << std::endl;
-            //prev();
+            prev();
             //std::cout << "2" << std::endl;
             ParseTree* pro = compileClass();
            // std::cout << "3" << std::endl;
@@ -653,9 +653,10 @@ void CompilerParser::next(){
 
 void CompilerParser::prev(){
     int i=0;
+    itNum--;
     for (Token* t : tokenList){
         if(i == itNum){
-            itNum--;
+            
             *it = t;
             return;
         }
